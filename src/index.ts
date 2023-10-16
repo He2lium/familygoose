d6c5f5c5b164ea2ed368e2ff63a1a86c449246d7
pavelprna <pavel@prna.ru>
2023-10-16 14:23:23 +0700

    fix: types and builder export

--- src/index.ts
+++ src/index.ts
@@ -1,48 +1,59 @@
-import { Schema } from 'mongoose'
-import { Relationship } from './types/Factory'
-import ManyToOneMiddlewares = Relationship.ManyToOneMiddlewares
-import ManyToManyMiddlewares = Relationship.ManyToManyMiddlewares
-import OneToManyMiddlewares = Relationship.OneToManyMiddlewares
+import { Schema } from "mongoose";
+import { Relationship } from "./types/Factory";
+import ManyToOneMiddlewares = Relationship.ManyToOneMiddlewares;
+import ManyToManyMiddlewares = Relationship.ManyToManyMiddlewares;
+import OneToManyMiddlewares = Relationship.OneToManyMiddlewares;
 
-export const FamilyGoose = (
-  schema: Schema,
-  options: (ManyToOneMiddlewares | ManyToManyMiddlewares | OneToManyMiddlewares)[]
+export { FG } from "./utils/RelationshipBuilder";
+
+export const FamilyGoose = <DocType>(
+  schema: Schema<DocType & any>,
+  options: (
+    | ManyToOneMiddlewares
+    | ManyToManyMiddlewares
+    | OneToManyMiddlewares
+  )[]
 ) => {
   for (let middlewares of options) {
     // Add local field to schema automatically
     if (middlewares.localField && !schema.path(middlewares.localField.key))
-      schema.add({ [`${middlewares.localField.key}`]: middlewares.localField.definition })
+      schema.add({
+        [`${middlewares.localField.key}`]: middlewares.localField.definition,
+      });
 
     // Set deleteMany generated middleware
     if (middlewares.preDeleteMany) {
-      schema.pre(['deleteMany', 'deleteOne'], middlewares.preDeleteMany)
+      schema.pre(["deleteMany", "deleteOne"], middlewares.preDeleteMany);
     }
 
     if (middlewares.postDeleteMany) {
-      schema.post(['deleteMany', 'deleteOne'], middlewares.postDeleteMany)
+      schema.post(["deleteMany", "deleteOne"], middlewares.postDeleteMany);
     }
 
     // Set saving generated middlewares
     if (middlewares.postSave) {
-      schema.post(['save'], middlewares.postSave)
+      schema.post(["save"], middlewares.postSave);
     }
 
     // Set updating generated middlewares
     if (middlewares.postUpdate) {
-      schema.post(['findOneAndUpdate'], middlewares.postUpdate)
+      schema.post(["findOneAndUpdate"], middlewares.postUpdate);
     }
 
     if (middlewares.preUpdateMany) {
-      schema.pre(['updateMany', 'updateOne'], middlewares.preUpdateMany)
+      schema.pre(["updateMany", "updateOne"], middlewares.preUpdateMany);
     }
 
     if (middlewares.postUpdateMany) {
-      schema.post(['updateMany', 'updateOne'], middlewares.postUpdateMany)
+      schema.post(["updateMany", "updateOne"], middlewares.postUpdateMany);
     }
 
     // Set destroying generated middlewares
     if (middlewares.postDelete) {
-      schema.post(['deleteOne', 'findOneAndDelete', 'findOneAndRemove'], middlewares.postDelete)
+      schema.post(
+        ["deleteOne", "findOneAndDelete", "findOneAndRemove"],
+        middlewares.postDelete
+      );
     }
   }
-}
+};
